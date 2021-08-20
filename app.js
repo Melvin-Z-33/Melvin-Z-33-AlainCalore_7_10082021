8d5641406d9820b11b873bc1d0feea8091ee7cf1
Melvin33 <melvin.marley@outlook.fr>
2021-08-20 18:29:27 +0200

    general display

--- app.js
+++ app.js
@@ -12,57 +12,56 @@ const fetchRecipes = async () => {
 
 //DISPLAY GENERAL INFO
 
-const showAllRecipes = async () => {
-	await fetchRecipes();
-	let displayRecipes = [];
-	let arrayForIngredients = [];
+// const showAllRecipes = async () => {
+// 	await fetchRecipes();
+// 	let displayRecipes = [];
+// 	let arrayForIngredients = [];
 
-	for (recipe of bookOfRecipes.recipes) {
-		for (objet of recipe.ingredients) {
-			if (
-				objet.hasOwnProperty('ingredient') &&
-				objet.hasOwnProperty('quantity') &&
-				objet.hasOwnProperty('unit')
-			) {
-				arrayForIngredients.push(
-					`<div>${objet.ingredient} : ${objet.quantity} ${objet.unit}</div> `,
-				);
-			} else if (objet.hasOwnProperty('ingredient') && objet.hasOwnProperty('quantity')) {
-				arrayForIngredients.push(`<div>${objet.ingredient} : ${objet.quantity} </div> `);
-			} else if (objet.hasOwnProperty('ingredient')) {
-				arrayForIngredients.push(`<div>${objet.ingredient}  </div> `);
-			}
-		}
+// 	for (recipe of bookOfRecipes.recipes) {
+// 		for (objet of recipe.ingredients) {
+// 			if (
+// 				objet.hasOwnProperty('ingredient') &&
+// 				objet.hasOwnProperty('quantity') &&
+// 				objet.hasOwnProperty('unit')
+// 			) {
+// 				arrayForIngredients.push(
+// 					`<div>${objet.ingredient} : ${objet.quantity} ${objet.unit}</div> `,
+// 				);
+// 			} else if (objet.hasOwnProperty('ingredient') && objet.hasOwnProperty('quantity')) {
+// 				arrayForIngredients.push(`<div>${objet.ingredient} : ${objet.quantity} </div> `);
+// 			} else if (objet.hasOwnProperty('ingredient')) {
+// 				arrayForIngredients.push(`<div>${objet.ingredient}  </div> `);
+// 			}
+// 		}
 
-		let a = `
-			<div class="card col col-md-4 mx-4 ">
-			<img class="" src="http://via.placeholder.com/10">
-
-		<div class="card-body container">
-			<div class="row justify-content-between">
-				<h4 class="card-title col-8">${recipe.name}</h4>
-				<p class="card-timer col-4 text-end"><i class="far fa-clock"></i> ${recipe.time} min</p>
-				<div class="card-ingredient col-6">
-					<p class='text-justify '>${arrayForIngredients.join('')}</p>
-				</div>
-				<div class="card-text col-6 text-start text-wrap">
-					<p>${recipe.description}</p>
-				</div>
-			</div>
-		</div>
-	</div>
-			`;
-		results.insertAdjacentHTML('beforeend', a);
-		arrayForIngredients = [];
-		a = '';
-	}
-};
+// 		let a = `
+// 			<div class="card col-md-4 mb-6 text center ">
+// 				<img class="" src="http://via.placeholder.com/10">
+// 				<div class="card-body container">
+// 					<div class=" justify-content-between">
+// 						<h2 class="card-title lato-bold col-8">${recipe.name}</h2>
+// 						<p class="card-timer lato-bold col-4 text-end"><i class="far fa-clock"></i> ${recipe.time} min</p>
+// 						<div class="card-ingredient col-6">
+// 							<p class='text-justify '>${arrayForIngredients.join('')}</p>
+// 						</div>
+// 						<div class="card-text col-6 text-start text-wrap">
+// 							<p>${recipe.description}</p>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 			`;
+// 		results.insertAdjacentHTML('beforeend', a);
+// 		arrayForIngredients = [];
+// 		a = '';
+// 	}
+// };
 
 //showAllRecipes();
 //SEARCH GENERAL
 const searchRecipes = async () => {
 	await fetchRecipes();
-	console.log(bookOfRecipes.recipes);
+
 	let arrayIngredients = [``];
 	let pieces;
 	results.innerHTML = bookOfRecipes.recipes
@@ -76,27 +75,25 @@ const searchRecipes = async () => {
 					ing.hasOwnProperty('quantity') &&
 					ing.hasOwnProperty('unit')
 				) {
-					foo += `<div>${ing.ingredient} : ${ing.quantity} ${ing.unit}</div> `;
+					foo += `<p><span class="lato-bold">${ing.ingredient}</span> : ${ing.quantity} ${ing.unit}</p> `;
 				} else if (ing.hasOwnProperty('ingredient') && ing.hasOwnProperty('quantity')) {
-					foo += `<div>${ing.ingredient} : ${ing.quantity} </div> `;
+					foo += `<p><span class="lato-bold">${ing.ingredient}</span> : ${ing.quantity} </p> `;
 				} else if (ing.hasOwnProperty('ingredient')) {
-					foo += `<div>${ing.ingredient}  </div> `;
+					foo += `<p><span class="lato-bold">${ing.ingredient}</span>  </p> `;
 				}
 			}
 
 			return `
-			<div class="card col col-md-3 mx-4 ">
-					<img class="" src="http://via.placeholder.com/10">
+			<div class="card  col-sm-12 col-md-3 mx-4 ">
+					<img class="" alt="" src="http://via.placeholder.com/10">
 
 				<div class="card-body container">
 					<div class="row justify-content-between">
-						<h4 class="card-title col-8">${recipe.name}</h4>
-						<p class="card-timer col-4 text-end"><i class="far fa-clock"></i> ${recipe.time} min</p>
-						<div class="card-ingredient col-6">
-							<p>${foo}</p>
-						</div>
-						<div class="card-text col-6 text-start text-wrap">
-							<p>${recipe.description}</p>
+						<h2  class="card-title lato-bold text-hidden col-8">${recipe.name}</h2>
+						<p class="card-timer lato-bold col-4 text-end"><i class="far fa-clock"></i> ${recipe.time} min</p>
+						<div class="card-ingredient  col-6">${foo}</div>
+						<div class="card-text   col-6 text-start ">
+							<p class=" description lato-regular ">${recipe.description}</p>
 						</div>
 					</div>
 				</div>
@@ -112,3 +109,20 @@ searchInput.addEventListener('input', (e) => {
 	searchRecipes();
 });
 searchRecipes();
+
+//** DROPDOWN */
+const comboIngredient = document.getElementById('combo-ingredient');
+
+const ingredients = async () => {
+	await fetchRecipes();
+	console.log(bookOfRecipes.recipes);
+	let tab = [];
+	for (let recipe of bookOfRecipes.recipes) {
+		for (let ingredient of recipe.ingredients) {
+			console.log(ingredient.ingredient);
+		}
+	}
+};
+comboIngredient.insertAdjacentHTML('beforeend', 'ff');
+
+ingredients();
